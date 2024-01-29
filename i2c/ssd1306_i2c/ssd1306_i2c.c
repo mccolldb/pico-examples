b0af6dbf8598c5b5e83a4f00357bb42525d0ac72
David McColl <dmccoll@sdccd.edu>
2024-01-28 20:08:38 -0800

    size 64x128 - reformat

--- i2c/ssd1306_i2c/ssd1306_i2c.c
+++ i2c/ssd1306_i2c/ssd1306_i2c.c
@@ -4,15 +4,15 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include "pico/stdlib.h"
-#include "pico/binary_info.h"
 #include "hardware/i2c.h"
+#include "pico/binary_info.h"
+#include "pico/stdlib.h"
 #include "raspberry26x32.h"
 #include "ssd1306_font.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /* Example code to talk to an SSD1306-based OLED display
 
@@ -38,54 +38,53 @@
 // Define the size of the display we have attached. This can vary, make sure you
 // have the right size defined or the output will look rather odd!
 // Code has been tested on 128x32 and 128x64 OLED displays
-#define SSD1306_HEIGHT              32
-#define SSD1306_WIDTH               128
+#define SSD1306_HEIGHT 64
+#define SSD1306_WIDTH 128
 
-#define SSD1306_I2C_ADDR            _u(0x3C)
+#define SSD1306_I2C_ADDR _u(0x3C)
 
 // 400 is usual, but often these can be overclocked to improve display response.
 // Tested at 1000 on both 32 and 84 pixel height devices and it worked.
-#define SSD1306_I2C_CLK             400
-//#define SSD1306_I2C_CLK             1000
-
+#define SSD1306_I2C_CLK 400
+// #define SSD1306_I2C_CLK             1000
 
 // commands (see datasheet)
-#define SSD1306_SET_MEM_MODE        _u(0x20)
-#define SSD1306_SET_COL_ADDR        _u(0x21)
-#define SSD1306_SET_PAGE_ADDR       _u(0x22)
-#define SSD1306_SET_HORIZ_SCROLL    _u(0x26)
-#define SSD1306_SET_SCROLL          _u(0x2E)
+#define SSD1306_SET_MEM_MODE _u(0x20)
+#define SSD1306_SET_COL_ADDR _u(0x21)
+#define SSD1306_SET_PAGE_ADDR _u(0x22)
+#define SSD1306_SET_HORIZ_SCROLL _u(0x26)
+#define SSD1306_SET_SCROLL _u(0x2E)
 
 #define SSD1306_SET_DISP_START_LINE _u(0x40)
 
-#define SSD1306_SET_CONTRAST        _u(0x81)
-#define SSD1306_SET_CHARGE_PUMP     _u(0x8D)
+#define SSD1306_SET_CONTRAST _u(0x81)
+#define SSD1306_SET_CHARGE_PUMP _u(0x8D)
 
-#define SSD1306_SET_SEG_REMAP       _u(0xA0)
-#define SSD1306_SET_ENTIRE_ON       _u(0xA4)
-#define SSD1306_SET_ALL_ON          _u(0xA5)
-#define SSD1306_SET_NORM_DISP       _u(0xA6)
-#define SSD1306_SET_INV_DISP        _u(0xA7)
-#define SSD1306_SET_MUX_RATIO       _u(0xA8)
-#define SSD1306_SET_DISP            _u(0xAE)
-#define SSD1306_SET_COM_OUT_DIR     _u(0xC0)
+#define SSD1306_SET_SEG_REMAP _u(0xA0)
+#define SSD1306_SET_ENTIRE_ON _u(0xA4)
+#define SSD1306_SET_ALL_ON _u(0xA5)
+#define SSD1306_SET_NORM_DISP _u(0xA6)
+#define SSD1306_SET_INV_DISP _u(0xA7)
+#define SSD1306_SET_MUX_RATIO _u(0xA8)
+#define SSD1306_SET_DISP _u(0xAE)
+#define SSD1306_SET_COM_OUT_DIR _u(0xC0)
 #define SSD1306_SET_COM_OUT_DIR_FLIP _u(0xC0)
 
-#define SSD1306_SET_DISP_OFFSET     _u(0xD3)
-#define SSD1306_SET_DISP_CLK_DIV    _u(0xD5)
-#define SSD1306_SET_PRECHARGE       _u(0xD9)
-#define SSD1306_SET_COM_PIN_CFG     _u(0xDA)
-#define SSD1306_SET_VCOM_DESEL      _u(0xDB)
+#define SSD1306_SET_DISP_OFFSET _u(0xD3)
+#define SSD1306_SET_DISP_CLK_DIV _u(0xD5)
+#define SSD1306_SET_PRECHARGE _u(0xD9)
+#define SSD1306_SET_COM_PIN_CFG _u(0xDA)
+#define SSD1306_SET_VCOM_DESEL _u(0xDB)
 
-#define SSD1306_PAGE_HEIGHT         _u(8)
-#define SSD1306_NUM_PAGES           (SSD1306_HEIGHT / SSD1306_PAGE_HEIGHT)
-#define SSD1306_BUF_LEN             (SSD1306_NUM_PAGES * SSD1306_WIDTH)
+#define SSD1306_PAGE_HEIGHT _u(8)
+#define SSD1306_NUM_PAGES (SSD1306_HEIGHT / SSD1306_PAGE_HEIGHT)
+#define SSD1306_BUF_LEN (SSD1306_NUM_PAGES * SSD1306_WIDTH)
 
-#define SSD1306_WRITE_MODE         _u(0xFE)
-#define SSD1306_READ_MODE          _u(0xFF)
+#define SSD1306_WRITE_MODE _u(0xFE)
+#define SSD1306_READ_MODE _u(0xFF)
 
-
-struct render_area {
+struct render_area
+{
     uint8_t start_col;
     uint8_t end_col;
     uint8_t start_page;
@@ -94,14 +93,16 @@ struct render_area {
     int buflen;
 };
 
-void calc_render_area_buflen(struct render_area *area) {
+void calc_render_area_buflen(struct render_area *area)
+{
     // calculate how long the flattened buffer will be for a render area
     area->buflen = (area->end_col - area->start_col + 1) * (area->end_page - area->start_page + 1);
 }
 
 #ifdef i2c_default
 
-void SSD1306_send_cmd(uint8_t cmd) {
+void SSD1306_send_cmd(uint8_t cmd)
+{
     // I2C write process expects a control byte followed by data
     // this "data" can be a command or data to follow up a command
     // Co = 1, D/C = 0 => the driver expects a command
@@ -109,12 +110,14 @@ void SSD1306_send_cmd(uint8_t cmd) {
     i2c_write_blocking(i2c_default, SSD1306_I2C_ADDR, buf, 2, false);
 }
 
-void SSD1306_send_cmd_list(uint8_t *buf, int num) {
-    for (int i=0;i<num;i++)
+void SSD1306_send_cmd_list(uint8_t *buf, int num)
+{
+    for (int i = 0; i < num; i++)
         SSD1306_send_cmd(buf[i]);
 }
 
-void SSD1306_send_buf(uint8_t buf[], int buflen) {
+void SSD1306_send_buf(uint8_t buf[], int buflen)
+{
     // in horizontal addressing mode, the column address pointer auto-increments
     // and then wraps around to the next page, so we can send the entire frame
     // buffer in one gooooooo!
@@ -125,24 +128,25 @@ void SSD1306_send_buf(uint8_t buf[], int buflen) {
     uint8_t *temp_buf = malloc(buflen + 1);
 
     temp_buf[0] = 0x40;
-    memcpy(temp_buf+1, buf, buflen);
+    memcpy(temp_buf + 1, buf, buflen);
 
     i2c_write_blocking(i2c_default, SSD1306_I2C_ADDR, temp_buf, buflen + 1, false);
 
     free(temp_buf);
 }
 
-void SSD1306_init() {
+void SSD1306_init()
+{
     // Some of these commands are not strictly necessary as the reset
     // process defaults to some of these but they are shown here
     // to demonstrate what the initialization sequence looks like
     // Some configuration values are recommended by the board manufacturer
 
     uint8_t cmds[] = {
-        SSD1306_SET_DISP,               // set display off
+        SSD1306_SET_DISP, // set display off
         /* memory mapping */
-        SSD1306_SET_MEM_MODE,           // set memory address mode 0 = horizontal, 1 = vertical, 2 = page
-        0x00,                           // horizontal addressing mode
+        SSD1306_SET_MEM_MODE, // set memory address mode 0 = horizontal, 1 = vertical, 2 = page
+        0x00,                 // horizontal addressing mode
         /* resolution and layout */
         SSD1306_SET_DISP_START_LINE,    // set display start line to 0
         SSD1306_SET_SEG_REMAP | 0x01,   // set segment re-map, column address 127 is mapped to SEG0
@@ -151,53 +155,55 @@ void SSD1306_init() {
         SSD1306_SET_COM_OUT_DIR | 0x08, // set COM (common) output scan direction. Scan from bottom up, COM[N-1] to COM0
         SSD1306_SET_DISP_OFFSET,        // set display offset
         0x00,                           // no offset
-        SSD1306_SET_COM_PIN_CFG,        // set COM (common) pins hardware configuration. Board specific magic number. 
+        SSD1306_SET_COM_PIN_CFG,        // set COM (common) pins hardware configuration. Board specific magic number.
                                         // 0x02 Works for 128x32, 0x12 Possibly works for 128x64. Other options 0x22, 0x32
 #if ((SSD1306_WIDTH == 128) && (SSD1306_HEIGHT == 32))
-        0x02,                           
+        0x02,
 #elif ((SSD1306_WIDTH == 128) && (SSD1306_HEIGHT == 64))
         0x12,
 #else
         0x02,
 #endif
         /* timing and driving scheme */
-        SSD1306_SET_DISP_CLK_DIV,       // set display clock divide ratio
-        0x80,                           // div ratio of 1, standard freq
-        SSD1306_SET_PRECHARGE,          // set pre-charge period
-        0xF1,                           // Vcc internally generated on our board
-        SSD1306_SET_VCOM_DESEL,         // set VCOMH deselect level
-        0x30,                           // 0.83xVcc
+        SSD1306_SET_DISP_CLK_DIV, // set display clock divide ratio
+        0x80,                     // div ratio of 1, standard freq
+        SSD1306_SET_PRECHARGE,    // set pre-charge period
+        0xF1,                     // Vcc internally generated on our board
+        SSD1306_SET_VCOM_DESEL,   // set VCOMH deselect level
+        0x30,                     // 0.83xVcc
         /* display */
-        SSD1306_SET_CONTRAST,           // set contrast control
+        SSD1306_SET_CONTRAST, // set contrast control
         0xFF,
-        SSD1306_SET_ENTIRE_ON,          // set entire display on to follow RAM content
-        SSD1306_SET_NORM_DISP,           // set normal (not inverted) display
-        SSD1306_SET_CHARGE_PUMP,        // set charge pump
-        0x14,                           // Vcc internally generated on our board
-        SSD1306_SET_SCROLL | 0x00,      // deactivate horizontal scrolling if set. This is necessary as memory writes will corrupt if scrolling was enabled
-        SSD1306_SET_DISP | 0x01, // turn display on
+        SSD1306_SET_ENTIRE_ON,     // set entire display on to follow RAM content
+        SSD1306_SET_NORM_DISP,     // set normal (not inverted) display
+        SSD1306_SET_CHARGE_PUMP,   // set charge pump
+        0x14,                      // Vcc internally generated on our board
+        SSD1306_SET_SCROLL | 0x00, // deactivate horizontal scrolling if set. This is necessary as memory writes will corrupt if scrolling was enabled
+        SSD1306_SET_DISP | 0x01,   // turn display on
     };
 
     SSD1306_send_cmd_list(cmds, count_of(cmds));
 }
 
-void SSD1306_scroll(bool on) {
+void SSD1306_scroll(bool on)
+{
     // configure horizontal scrolling
     uint8_t cmds[] = {
         SSD1306_SET_HORIZ_SCROLL | 0x00,
-        0x00, // dummy byte
-        0x00, // start page 0
-        0x00, // time interval
-        0x03, // end page 3 SSD1306_NUM_PAGES ??
-        0x00, // dummy byte
-        0xFF, // dummy byte
+        0x00,                                // dummy byte
+        0x00,                                // start page 0
+        0x00,                                // time interval
+        0x03,                                // end page 3 SSD1306_NUM_PAGES ??
+        0x00,                                // dummy byte
+        0xFF,                                // dummy byte
         SSD1306_SET_SCROLL | (on ? 0x01 : 0) // Start/stop scrolling
     };
 
     SSD1306_send_cmd_list(cmds, count_of(cmds));
 }
 
-void render(uint8_t *buf, struct render_area *area) {
+void render(uint8_t *buf, struct render_area *area)
+{
     // update a portion of the display with a render area
     uint8_t cmds[] = {
         SSD1306_SET_COL_ADDR,
@@ -205,15 +211,15 @@ void render(uint8_t *buf, struct render_area *area) {
         area->end_col,
         SSD1306_SET_PAGE_ADDR,
         area->start_page,
-        area->end_page
-    };
-    
+        area->end_page};
+
     SSD1306_send_cmd_list(cmds, count_of(cmds));
     SSD1306_send_buf(buf, area->buflen);
 }
 
-static void SetPixel(uint8_t *buf, int x,int y, bool on) {
-    assert(x >= 0 && x < SSD1306_WIDTH && y >=0 && y < SSD1306_HEIGHT);
+static void SetPixel(uint8_t *buf, int x, int y, bool on)
+{
+    assert(x >= 0 && x < SSD1306_WIDTH && y >= 0 && y < SSD1306_HEIGHT);
 
     // The calculation to determine the correct bit to set depends on which address
     // mode we are in. This code assumes horizontal
@@ -225,104 +231,117 @@ static void SetPixel(uint8_t *buf, int x,int y, bool on) {
     // This code could be optimised, but is like this for clarity. The compiler
     // should do a half decent job optimising it anyway.
 
-    const int BytesPerRow = SSD1306_WIDTH ; // x pixels, 1bpp, but each row is 8 pixel high, so (x / 8) * 8
+    const int BytesPerRow = SSD1306_WIDTH; // x pixels, 1bpp, but each row is 8 pixel high, so (x / 8) * 8
 
     int byte_idx = (y / 8) * BytesPerRow + x;
     uint8_t byte = buf[byte_idx];
 
     if (on)
-        byte |=  1 << (y % 8);
+        byte |= 1 << (y % 8);
     else
         byte &= ~(1 << (y % 8));
 
     buf[byte_idx] = byte;
 }
 // Basic Bresenhams.
-static void DrawLine(uint8_t *buf, int x0, int y0, int x1, int y1, bool on) {
+static void DrawLine(uint8_t *buf, int x0, int y0, int x1, int y1, bool on)
+{
 
-    int dx =  abs(x1-x0);
-    int sx = x0<x1 ? 1 : -1;
-    int dy = -abs(y1-y0);
-    int sy = y0<y1 ? 1 : -1;
-    int err = dx+dy;
+    int dx = abs(x1 - x0);
+    int sx = x0 < x1 ? 1 : -1;
+    int dy = -abs(y1 - y0);
+    int sy = y0 < y1 ? 1 : -1;
+    int err = dx + dy;
     int e2;
 
-    while (true) {
+    while (true)
+    {
         SetPixel(buf, x0, y0, on);
         if (x0 == x1 && y0 == y1)
             break;
-        e2 = 2*err;
+        e2 = 2 * err;
 
-        if (e2 >= dy) {
+        if (e2 >= dy)
+        {
             err += dy;
             x0 += sx;
         }
-        if (e2 <= dx) {
+        if (e2 <= dx)
+        {
             err += dx;
             y0 += sy;
         }
     }
 }
 
-static inline int GetFontIndex(uint8_t ch) {
-    if (ch >= 'A' && ch <='Z') {
-        return  ch - 'A' + 1;
+static inline int GetFontIndex(uint8_t ch)
+{
+    if (ch >= 'A' && ch <= 'Z')
+    {
+        return ch - 'A' + 1;
     }
-    else if (ch >= '0' && ch <='9') {
-        return  ch - '0' + 27;
+    else if (ch >= '0' && ch <= '9')
+    {
+        return ch - '0' + 27;
     }
-    else return  0; // Not got that char so space.
+    else
+        return 0; // Not got that char so space.
 }
 
 static uint8_t reversed[sizeof(font)] = {0};
 
-static uint8_t reverse(uint8_t b) {
-   b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
-   b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
-   b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
-   return b;
+static uint8_t reverse(uint8_t b)
+{
+    b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
+    b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
+    b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
+    return b;
 }
-static void FillReversedCache() {
+static void FillReversedCache()
+{
     // calculate and cache a reversed version of fhe font, because I defined it upside down...doh!
-    for (int i=0;i<sizeof(font);i++)
+    for (int i = 0; i < sizeof(font); i++)
         reversed[i] = reverse(font[i]);
 }
 
-static void WriteChar(uint8_t *buf, int16_t x, int16_t y, uint8_t ch) {
-    if (reversed[0] == 0) 
+static void WriteChar(uint8_t *buf, int16_t x, int16_t y, uint8_t ch)
+{
+    if (reversed[0] == 0)
         FillReversedCache();
 
     if (x > SSD1306_WIDTH - 8 || y > SSD1306_HEIGHT - 8)
         return;
 
     // For the moment, only write on Y row boundaries (every 8 vertical pixels)
-    y = y/8;
+    y = y / 8;
 
     ch = toupper(ch);
     int idx = GetFontIndex(ch);
     int fb_idx = y * 128 + x;
 
-    for (int i=0;i<8;i++) {
+    for (int i = 0; i < 8; i++)
+    {
         buf[fb_idx++] = reversed[idx * 8 + i];
     }
 }
 
-static void WriteString(uint8_t *buf, int16_t x, int16_t y, char *str) {
+static void WriteString(uint8_t *buf, int16_t x, int16_t y, char *str)
+{
     // Cull out any string off the screen
     if (x > SSD1306_WIDTH - 8 || y > SSD1306_HEIGHT - 8)
         return;
 
-    while (*str) {
+    while (*str)
+    {
         WriteChar(buf, x, y, *str++);
-        x+=8;
+        x += 8;
     }
 }
 
-
-
 #endif
 
-int main() {
+int main()
+{
     stdio_init_all();
 
 #if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
@@ -348,11 +367,11 @@ int main() {
 
     // Initialize render area for entire frame (SSD1306_WIDTH pixels by SSD1306_NUM_PAGES pages)
     struct render_area frame_area = {
-        start_col: 0,
+        start_col : 0,
         end_col : SSD1306_WIDTH - 1,
         start_page : 0,
         end_page : SSD1306_NUM_PAGES - 1
-        };
+    };
 
     calc_render_area_buflen(&frame_area);
 
@@ -362,8 +381,9 @@ int main() {
     render(buf, &frame_area);
 
     // intro sequence: flash the screen 3 times
-    for (int i = 0; i < 3; i++) {
-        SSD1306_send_cmd(SSD1306_SET_ALL_ON);    // Set all pixels on
+    for (int i = 0; i < 3; i++)
+    {
+        SSD1306_send_cmd(SSD1306_SET_ALL_ON); // Set all pixels on
         sleep_ms(500);
         SSD1306_send_cmd(SSD1306_SET_ENTIRE_ON); // go back to following RAM for pixel state
         sleep_ms(500);
@@ -372,7 +392,7 @@ int main() {
     // render 3 cute little raspberries
     struct render_area area = {
         start_page : 0,
-        end_page : (IMG_HEIGHT / SSD1306_PAGE_HEIGHT)  - 1
+        end_page : (IMG_HEIGHT / SSD1306_PAGE_HEIGHT) - 1
     };
 
 restart:
@@ -381,10 +401,11 @@ restart:
     area.end_col = IMG_WIDTH - 1;
 
     calc_render_area_buflen(&area);
-    
+
     uint8_t offset = 5 + IMG_WIDTH; // 5px padding
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++)
+    {
         render(raspberry26x32, &area);
         area.start_col += offset;
         area.end_col += offset;
@@ -402,13 +423,13 @@ restart:
         "Lived a small",
         "red raspberry",
         "by the name of",
-        "    PICO"
-    };
+        "    PICO"};
 
     int y = 0;
-    for (int i = 0 ;i < count_of(text); i++) {
+    for (int i = 0; i < count_of(text); i++)
+    {
         WriteString(buf, 5, y, text[i]);
-        y+=8;
+        y += 8;
     }
     render(buf, &frame_area);
 
@@ -419,13 +440,16 @@ restart:
     SSD1306_send_cmd(SSD1306_SET_NORM_DISP);
 
     bool pix = true;
-    for (int i = 0; i < 2;i++) {
-        for (int x = 0;x < SSD1306_WIDTH;x++) {
-            DrawLine(buf, x, 0,  SSD1306_WIDTH - 1 - x, SSD1306_HEIGHT - 1, pix);
+    for (int i = 0; i < 2; i++)
+    {
+        for (int x = 0; x < SSD1306_WIDTH; x++)
+        {
+            DrawLine(buf, x, 0, SSD1306_WIDTH - 1 - x, SSD1306_HEIGHT - 1, pix);
             render(buf, &frame_area);
         }
 
-        for (int y = SSD1306_HEIGHT-1; y >= 0 ;y--) {
+        for (int y = SSD1306_HEIGHT - 1; y >= 0; y--)
+        {
             DrawLine(buf, 0, y, SSD1306_WIDTH - 1, SSD1306_HEIGHT - 1 - y, pix);
             render(buf, &frame_area);
         }
